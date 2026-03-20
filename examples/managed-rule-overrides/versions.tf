5b790b29fb3740a410990cfc951daca6b4249042
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 15:55:28 -0400

    chore: polish module with basic example, changelog, and version fixes (#12)
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

--- examples/managed-rule-overrides/versions.tf
+++ examples/managed-rule-overrides/versions.tf
@@ -1,16 +1,10 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
   required_version = ">= 1.10.0"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.116.0"
+      version = "# Terraform version"
     }
   }
 }
