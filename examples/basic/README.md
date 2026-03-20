a117ebdf754faea9bbd15917b3198ac61714499f
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 15:55:24 -0400

    chore: polish module with basic example, changelog, and version fixes

---
+++ examples/basic/README.md
@@ -0,0 +1,8 @@
+# Basic Example
+
+Minimal example for the terraform-azurerm-waf module.
+
+```bash
+terraform init
+terraform plan
+```
--- versions.tf
+++ versions.tf
@@ -1,19 +1,12 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
   required_version = ">= 1.10.0"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.116.0"
+      version = "# Terraform version"
     }
   }
-
   provider_meta "azurerm" {
     module_name = "terraform-az-modules/terraform-azurerm-waf"
   }
